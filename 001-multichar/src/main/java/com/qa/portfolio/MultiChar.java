0b03cd25b5e4e408b67c9f37a5647762a4ae60bd
Becca Staple <petite.becca.s@gmail.com>
2020-05-27 19:27:25 +0100

    modified 001-MultiChar - test passed

--- 001-multichar/src/main/java/com/qa/portfolio/MultiChar.java
+++ 001-multichar/src/main/java/com/qa/portfolio/MultiChar.java
@@ -12,7 +12,35 @@ public class MultiChar {
 	 */
 	public String multiChar(String input) {
 		// TODO
-		return "";
+		//String subString = input.substring(0 * 3,1);
+		
+		
+//		for (int i = 0; i < input.length(); i++) {
+//			if (input.substring(i, i++).matches(input.substring(i))) {
+//				System.out.println(input.substring(i, i++) + input.substring(i, i++));
+//			} else {
+//				System.out.println(input.substring(i, i++));
+//			}
+//		}
+		StringBuilder strbld = new StringBuilder(input.length());
+		for (int i = 0; i < input.length(); i++) {
+			char a = input.charAt(i);
+			strbld.append(a);
+			if (Character.isLetter(a)) {
+				strbld.append(a);
+			}
+			if (Character.isLetter(a)) {
+				strbld.append(a);
+			}
+			if (a == '-') {
+				strbld.append(a).append(a);
+			}
+		}
+		return strbld.toString();
+		
+		
+		
+		
 	}
 
 }
