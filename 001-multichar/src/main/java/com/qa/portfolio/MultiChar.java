165374d71ad0d2a9c5d6e91261e21214fe7e482d
Becca Staple <petite.becca.s@gmail.com>
2020-05-27 19:30:50 +0100

    took out commented out code

--- 001-multichar/src/main/java/com/qa/portfolio/MultiChar.java
+++ 001-multichar/src/main/java/com/qa/portfolio/MultiChar.java
@@ -12,16 +12,7 @@ public class MultiChar {
 	 */
 	public String multiChar(String input) {
 		// TODO
-		//String subString = input.substring(0 * 3,1);
 		
-		
-//		for (int i = 0; i < input.length(); i++) {
-//			if (input.substring(i, i++).matches(input.substring(i))) {
-//				System.out.println(input.substring(i, i++) + input.substring(i, i++));
-//			} else {
-//				System.out.println(input.substring(i, i++));
-//			}
-//		}
 		StringBuilder strbld = new StringBuilder(input.length());
 		for (int i = 0; i < input.length(); i++) {
 			char a = input.charAt(i);
